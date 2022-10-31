10c1d5174d46f6af75149ccdc90df126aba885b4
LoginamNet <loginamnet@gmail.com>
2022-10-31 12:40:04 +0300

    init: start 02 task solution

---
+++ 02-write-file/index.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const fs = require('fs');
+
+const pathToText = path.join(__dirname, 'text.txt');
+const text = fs.createWriteStream(pathToText);
+
+function textInput() {
+    process.stdout.write(`Введите любой текст: `);
+
+    process.stdin.on('data', (data) => {
+
+        if (data.toString().trim() === `exit`) {
+            process.stdout.write(`Вы вышли из режима ввода`);
+            process.exit();
+        } else {
+            text.write(data.toString());
+            process.stdout.write(`Ваш текст "${data.toString()}" был записан в файл text.txt\n`);
+            process.stdout.write(`Введите любой текст: `);
+        }
+    })
+
+}
+
+textInput();
