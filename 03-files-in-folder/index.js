01eb41e49917e1b2f772e80a35c0ec053dd80c32
LoginamNet <loginamnet@gmail.com>
2022-10-31 18:45:31 +0300

    reafactor: refactor with try catch

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -4,24 +4,31 @@ const {readdir, stat} = require('fs/promises')
 const pathToFolder = path.join(__dirname, 'secret-folder');
 
 async function readFiles() {
-    const folderItems = await readdir(pathToFolder);
+    
+    try {
 
-    console.log(folderItems);
+        const folderItems = await readdir(pathToFolder);
 
-    folderItems.forEach(async (item) => {
-        const pathToFile = path.join(pathToFolder, item);
-        const fileStats = await stat(pathToFile);
+        console.log(folderItems);
+    
+        folderItems.forEach(async (item) => {
+            const pathToFile = path.join(pathToFolder, item);
+            const fileStats = await stat(pathToFile);
+    
+            if (fileStats.isFile()) {
+                const fileExpLength =  path.extname(pathToFile).length;
+                const fileBaseLenth = path.basename(pathToFile).length;
+    
+                const fileName = path.basename(pathToFile).slice(0, fileBaseLenth - fileExpLength);
+                const fileExp = path.extname(pathToFile).slice(-fileExpLength + 1);
+                const fileSize = Math.round(fileStats.size / 1024 * 100) / 100;
+                console.log(`${fileName} - ${fileExp} - ${fileSize}kb`);
+            }
+        })
 
-        if (fileStats.isFile()) {
-            const fileExpLength =  path.extname(pathToFile).length;
-            const fileBaseLenth = path.basename(pathToFile).length;
-
-            const fileName = path.basename(pathToFile).slice(0, fileBaseLenth - fileExpLength);
-            const fileExp = path.extname(pathToFile).slice(-fileExpLength + 1);
-            const fileSize = Math.round(fileStats.size / 1024 * 100) / 100;
-            console.log(`${fileName} - ${fileExp} - ${fileSize}kb`);
-        }
-    })
+    } catch (err) {
+        console.error(err);
+    }
 
 }
 
