05a64b143faa37c7b20b1716a93b06775aba187e
LoginamNet <loginamnet@gmail.com>
2022-11-01 22:51:26 +0300

    feat: add 05 solution

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const {writeFile, readFile, appendFile, readdir, stat} = require('fs/promises');
+
+
+async function createFile() {
+    try {
+        await writeFile(path.join('05-merge-styles/project-dist', 'bundle.css'), '');
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+async function readData(pathToStyleFile, pathToBundleFile) {
+    try {
+        let data = await readFile(pathToStyleFile, 'utf8');
+        await appendFile(pathToBundleFile, data);
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+async function createBundle() {
+    createFile();
+
+    const pathToStyleFolder = path.join(__dirname, 'styles'); 
+    const pathToBundleFile = path.join(__dirname, 'project-dist', 'bundle.css');
+    const stylefolderItems = await readdir(pathToStyleFolder);
+
+    stylefolderItems.forEach(async (item) => {
+        const pathToStyleFile = path.join(pathToStyleFolder, item);
+        const fileStats = await stat(pathToStyleFile);
+        const fileExp = path.extname(pathToStyleFile);
+
+        if(fileStats.isFile() && fileExp === '.css') {
+            await readData(pathToStyleFile, pathToBundleFile);
+        }
+
+    })
+}
+
+createBundle();
