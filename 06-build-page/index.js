988b3b6f7dd0915f902c86d82dc11677c3d8b510
LoginamNet <loginamnet@gmail.com>
2022-11-02 14:28:06 +0300

    feat: add 05 silution

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,5 +1,14 @@
+const fs = require('fs');
 const path = require('path');
-const {mkdir, readdir, unlink, stat, copyFile} = require('fs/promises');
+const {mkdir, readdir, unlink, stat, copyFile, writeFile, readFile, appendFile} = require('fs/promises');
+
+async function createFile(pathToFile) {
+    try {
+        await writeFile(pathToFile, '');
+    } catch (err) {
+        console.error(err);
+    }
+}
 
 async function createFolder(pathToFolder) {
     try {
@@ -19,6 +28,92 @@ async function clearFolder(pathToFolder) {
 
             if (fileStats.isFile()) {
                 await unlink(pathToFile);
+            }
+        })
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+async function createHtmlBundle(pathToHtmlBundleFile, pathToComponentsFolder) {
+    try {
+        createFile(pathToHtmlBundleFile);
+
+        const componentsfolderItems = await readdir(pathToComponentsFolder);
+        let htmlData = await readFile(path.join(__dirname, 'template.html'), 'utf-8');
+
+        /* componentsfolderItems.forEach(async (item) => {
+
+            const pathToFile = path.join(pathToComponentsFolder, item);
+            const fileExpLength =  path.extname(pathToFile).length;
+            const fileBaseLenth = path.basename(pathToFile).length;
+            const fileName = path.basename(pathToFile).slice(0, fileBaseLenth - fileExpLength);
+            
+            const replacer = await readFile(pathToFile, 'utf-8');
+
+            htmlData = htmlData.replace(`{{${fileName}}}`, replacer);
+            console.log(htmlData)
+            
+        }) */
+
+        for (let item of componentsfolderItems) {
+            const pathToFile = path.join(pathToComponentsFolder, item);
+            const fileExpLength =  path.extname(pathToFile).length;
+            const fileBaseLenth = path.basename(pathToFile).length;
+            const fileName = path.basename(pathToFile).slice(0, fileBaseLenth - fileExpLength);
+            
+            const replacer = await readFile(pathToFile, 'utf-8');
+
+            htmlData = htmlData.replace(`{{${fileName}}}`, replacer);
+        }
+
+
+
+        await writeFile(pathToHtmlBundleFile, htmlData);
+
+
+
+        /* let str = await fs.promises.readFile(path.join(__dirname, 'template.html'), 'utf-8');
+        const component1 = await fs.promises.readFile(path.join(__dirname, 'components', 'header.html'), 'utf-8');
+        str = str.replace(`{{header}}`, component1);
+        const component2 = await fs.promises.readFile(path.join(__dirname, 'components', 'articles.html'), 'utf-8');
+        str = str.replace(`{{articles}}`, component2);
+        const component3 = await fs.promises.readFile(path.join(__dirname, 'components', 'footer.html'), 'utf-8');
+        str = str.replace(`{{footer}}`, component3);
+
+        const writeStr = fs.createWriteStream(path.join(__dirname, 'project-dist', 'index.html'));
+        writeStr.write(str); */
+
+    } catch (error) {
+        console.log(error.message)
+    }
+
+}
+
+async function appendStyles(pathToStyleFile, pathToBundleFile) {
+    try {
+        let data = await readFile(pathToStyleFile, 'utf8');
+        await appendFile(pathToBundleFile, `${data}\n`);
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+async function createStylesBundle(pathToStyleBundleFile, pathToStylesFolder) {
+    try {
+        createFile(pathToStyleBundleFile);
+
+        const stylefolderItems = await readdir(pathToStylesFolder);
+
+        stylefolderItems.forEach(async (item) => {
+            const pathToStyleFile = path.join(pathToStylesFolder, item);
+
+            const fileStats = await stat(pathToStyleFile);
+            const fileExp = path.extname(pathToStyleFile);
+
+            if (fileStats.isFile() && fileExp === '.css') {
+                await appendStyles(pathToStyleFile, pathToStyleBundleFile);
+                console.log(`Стили из файла ${item} были добавлены в файл style.css`);
             }
         })
     } catch (err) {
@@ -70,15 +165,23 @@ async function copyAssets(pathToInitFolder, pathToDestFolder) {
 
 async function bundleProject() {
 
-    const pathToInitFolder = path.join(__dirname, 'assets');
     const pathToProjectFolder = path.join(__dirname, 'project-dist');
+
+    const pathToComponentsFolder = path.join(__dirname, 'components');
+    const pathToHtmlBundleFile = path.join(pathToProjectFolder, 'index.html');
+
     const pathToAssetsFolder = path.join(__dirname, 'assets');
     const pathToCopyAssetsFolder = path.join(pathToProjectFolder, 'assets');
 
+    const pathToStylesFolder = path.join(__dirname, 'styles'); 
+    const pathToStyleBundleFile = path.join(pathToProjectFolder, 'style.css');
+
     createFolder(pathToProjectFolder);
-    clearFolder(pathToProjectFolder);
+    await clearFolder(pathToProjectFolder);
 
     copyAssets(pathToAssetsFolder, pathToCopyAssetsFolder);
+    createStylesBundle(pathToStyleBundleFile, pathToStylesFolder);
+    createHtmlBundle(pathToHtmlBundleFile, pathToComponentsFolder);
 
 
 
